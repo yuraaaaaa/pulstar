00d17899f3f54d2d091f43e2e8dca16d1945b9b8
yuraaaaaa <wsubota@gmail.com>
2022-05-14 15:04:29 +0300

    finish pulstar

--- gulp/tasks/copy.js
+++
@@ -1,9 +0,0 @@
-export const copy = () => {
-    return app.gulp.src(app.path.src.files)
-        .pipe(app.gulp.dest(app.path.build.files))
-}
-
-export const copyCSS = () => {
-    return app.gulp.src(app.path.src.css)
-        .pipe(app.gulp.dest(app.path.build.css))
-}
